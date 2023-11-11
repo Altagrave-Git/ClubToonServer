fc3412f536267d145941e7c3ce4dce5d0ea1cc36
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-10 17:16:29 -0700

    Custom auth middleware wrapper

--- main/asgi.py
+++ main/asgi.py
@@ -1,9 +1,8 @@
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from .middlewares import TokenAuthMiddleware
 from .routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
@@ -14,7 +13,7 @@ application = ProtocolTypeRouter(
     {
         'http': django_asgi_app,
         'websocket': AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            TokenAuthMiddleware(URLRouter(websocket_urlpatterns))
         )
     }
 )
