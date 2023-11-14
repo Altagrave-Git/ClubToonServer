e3b8b023c7f9e28214c64bb4877866b71427e5e4
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-13 21:27:16 -0700

    Token -> authtoken, create avatar on create user

--- users/signals.py
+++ users/signals.py
@@ -1,12 +1,12 @@
 from django.dispatch import receiver
-from users.models import User
+from users.models import User, Avatar
 from django.db.models.signals import post_save
-from rest_framework.authtoken.models import Token
+from knox.models import AuthToken
  
  
-# create(user) => create(jwt)
+# create(user) => create(token)
 @receiver(post_save, sender=User)
 def create_token(sender, instance, created, **kwargs):
     if created:
-        token = Token.objects.create(user=instance)
-        token.save()
+        AuthToken.objects.create(user=instance)
+        Avatar.objects.create(user=instance)
