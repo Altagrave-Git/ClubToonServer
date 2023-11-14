8abb0ed15aad217c8efb4062cef57c25c99479e8
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-13 21:14:23 -0700

    Nest socket url routers

--- main/routing.py
+++ main/routing.py
@@ -1,6 +1,7 @@
-from django.urls import include, re_path
-
+from django.urls import re_path
+from channels.routing import URLRouter
+from users.routing import websocket_urlpatterns as users_urlpatterns
 
 websocket_urlpatterns = [
-    re_path(r'ws/users/', include('users.routing')),
+    re_path(r'ws/users/', URLRouter(users_urlpatterns)),
 ]
