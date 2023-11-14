2207dcabbcc9a391181e2b860a7fc859dd5d0463
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-13 21:27:36 -0700

    Add register, logout, logoutall urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from knox.views import LogoutView, LogoutAllView
 
 
 urlpatterns = [
-    path('login/', views.login_view, name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logoutall/', LogoutAllView.as_view(), name='logoutall'),
 ]
