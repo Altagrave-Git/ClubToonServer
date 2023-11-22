aebb755d14af9ba7ed6cfcdfab397ef82d7fb62f
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-21 23:16:24 -0700

    Add all avatar models to admin

---
+++ avatar/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Avatar)
+admin.site.register(models.Owned)
+admin.site.register(models.Skin)
+admin.site.register(models.Hair)
+admin.site.register(models.Eyes)
+admin.site.register(models.Mouth)
+admin.site.register(models.Torso)
+admin.site.register(models.Hands)
+admin.site.register(models.Legs)
+admin.site.register(models.Feet)
